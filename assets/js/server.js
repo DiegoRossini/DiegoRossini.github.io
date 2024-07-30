55b818b304a117dd69dabb419ea5aed438e088ff
DiegoRossini <diego.rossini418@gmail.com>
2024-07-30 16:01:31 +0200

    Upedate server.js

--- assets/js/server.js
+++ assets/js/server.js
@@ -6,6 +6,7 @@ require('dotenv').config({ path: path.resolve(__dirname, '../../.env.txt') });
 const app = express();
 const port = 3000;
 
+// Middleware to parse JSON request bodies
 app.use(express.json());
 
 // Serve the HTML form at the root URL
@@ -13,8 +14,12 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../index.html'));
 });
 
+// Handle POST requests to /send-email
 app.post('/send-email', (req, res) => {
     const { name, email, message } = req.body;
+
+    console.log('Received POST request to /send-email');
+    console.log('Form data:', { name, email, message });
 
     const transporter = nodemailer.createTransport({
         service: 'gmail',
@@ -33,7 +38,7 @@ app.post('/send-email', (req, res) => {
 
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
-            console.error('Error:', error);
+            console.error('Error sending email:', error);
             res.status(500).send('Failed to send email');
         } else {
             console.log('Email sent:', info.response);
@@ -42,6 +47,7 @@ app.post('/send-email', (req, res) => {
     });
 });
 
+// Start the server
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server running at http://0.0.0.0:${port}/`);
 });
